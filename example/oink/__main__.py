3229cb463d593356e87cbcc065d5fe2514241a2d
Russell Anderson <5637107+rpanderson@users.noreply.github.com>
2020-05-12 12:46:09 +1000

    Display AppUserModelID for shortcut and process of the example app

--- example/oink/__main__.py
+++ example/oink/__main__.py
@@ -1,9 +1,11 @@
 import sys
 import tkinter
 import desktop_app
+from desktop_app.shell import _ModuleConfig
+from win32com.shell import shell
 
 desktop_app.set_process_appid("oink")
-
+appid = shell.GetCurrentProcessExplicitAppUserModelID()
 
 def main():
     root = tkinter.Tk(className=sys.argv[0])
@@ -13,7 +15,9 @@ def main():
     sys_info = (
         f"sys.prefix: {sys.prefix}\n"
         f"sys.exec_prefix: {sys.exec_prefix}\n"
-        f"sys.executable: {sys.executable}"
+        f"sys.executable: {sys.executable}\n"
+        f"AppUserModelID (shortcut): {_ModuleConfig.instance('oink').appid}\n"
+        f"AppUserModelID (process) : {appid}"
     )
     s = tkinter.Label(root, text=sys_info, justify=tkinter.LEFT)
     s.place(relx=0.05, rely=0.5, anchor=tkinter.W)
