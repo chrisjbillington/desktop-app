d195b1679027d245b0f6b300ac81685e4ed947be
chrisjbillington <chrisjbillington@gmail.com>
2020-05-18 18:27:32 -0400

    Declarative config

--- desktop_app/__init__.py
+++ desktop_app/__init__.py
@@ -1,24 +1,6 @@
-import setuptools_scm
-from pathlib import Path
-
-try:
-    import importlib.metadata as importlib_metadata
-except ImportError:
-    import importlib_metadata
-
-try:
-    __version__ = setuptools_scm.get_version(Path(__file__).parent.parent)
-except LookupError:
-    try:
-        __version__ = importlib_metadata.version(__name__)
-    except importlib_metadata.PackageNotFoundError:
-        __version__ = None
-
-
+from .__version__ import __version__
 from .launcher import entry_point
-
 from .shell import set_process_appid, install, uninstall
-
 
 __all__ = [
     'entry_point',
--- setup.py
+++ setup.py
@@ -1,54 +1,31 @@
 import os
 from setuptools import setup, Extension
 import platform
-WINDOWS = platform.system() == 'Windows'
 
 try:
     from setuptools_conda import dist_conda
+    CMDCLASS = {"dist_conda": dist_conda}
 except ImportError:
-    dist_conda = None
+    CMDCLASS = {}
 
-INSTALL_REQUIRES = [
-    "setuptools_scm",
-    "pywin32;               sys_platform == 'win32'",
-    "importlib_metadata;    python_version < '3.8'",
-]
+VERSION_SCHEME = {
+    "version_scheme": os.getenv("SCM_VERSION_SCHEME", "guess-next-dev"),
+    "local_scheme": os.getenv("SCM_LOCAL_SCHEME", "node-and-date"),
+}
 
-wineventhook = Extension(
-    'desktop_app.wineventhook',
-    sources=[os.path.join('src', 'wineventhook.cpp')],
-    libraries=["user32", "shell32", "ole32"],
-)
+if platform.system() == 'Windows':
+    EXT_MODULES = [
+        Extension(
+            'desktop_app.wineventhook',
+            sources=[os.path.join('src', 'wineventhook.cpp')],
+            libraries=["user32", "shell32", "ole32"],
+        )
+    ]
+else:
+    EXT_MODULES = []
 
 setup(
-    name='desktop-app',
-    use_scm_version=(
-        {'local_scheme': 'no-local-version'} if os.getenv('GITHUB_ACTIONS') else True
-    ),
-    description=(
-        "OS menu shortcuts, correct taskbar behaviour, and environment "
-        + "activation for Python GUI apps"
-    ),
-    long_description=open('README.md', encoding='utf8').read(),
-    long_description_content_type='text/markdown',
-    author='Chris Billington',
-    author_email='chrisjbillington@gmail.com ',
-    url='http://github.com/chrisjbillington/desktop-app',
-    license="BSD",
-    packages=["desktop_app"],
-    ext_modules=[wineventhook] if WINDOWS else [],
-    zip_safe=False,
-    setup_requires=['setuptools', 'setuptools_scm'],
-    include_package_data=True,
-    python_requires=">=3.6",
-    install_requires=INSTALL_REQUIRES if 'CONDA_BUILD' not in os.environ else [],
-    entry_points={'console_scripts': ['desktop-app = desktop_app.__main__:main',],},
-    cmdclass={'dist_conda': dist_conda} if dist_conda is not None else {},
-    command_options={
-        'dist_conda': {
-            'pythons': (__file__, ['3.6', '3.7', '3.8']),
-            'platforms': (__file__, ['linux-64', 'win-32', 'win-64', 'osx-64']),
-            'force_conversion': (__file__, True),
-        },
-    },
+    use_scm_version=VERSION_SCHEME,
+    ext_modules=EXT_MODULES,
+    cmdclass=CMDCLASS,
 )
